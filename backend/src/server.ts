a265d42c17f6e51a789644ef4123e051d56cd6d5
Devansh Behl <xxjm5q72pb@privaterelay.appleid.com>
2026-03-28 01:47:14 +0530

    working server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,18 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import cors from "cors";
+import app from "./app.js";
+import { env } from "./config/env.js";
+import { prisma } from "./lib/prisma.js"
 
-const app = express();
-app.use(express.json());
-
-
-
-app.listen(3000, () => {
-    console.log("server is running");
-});
+const startServer = async () => {
+    try {
+        await prisma.$connect();
+        console.log("DATABASE CONNECTED");
+        app.listen(env.PORT, () => {
+            console.log("Server Running");
+        })
+    } catch (e) {
+        console.log("failed to start server", e);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+startServer();
